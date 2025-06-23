1387cb49048569b9dda7b47b3d6d417fcbfa9a30
Emily <e.spiers13@gmail.com>
2025-06-23 12:57:45 +0100

    fixed index

--- db/index.js
+++ db/index.js
@@ -1,7 +1,5 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
-
 const ENV = process.env.NODE_ENV || "development";
 const pathToFile = `${__dirname}/../.env.${ENV}`;
 
@@ -18,5 +16,8 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
+// Optional: console log for debugging
+console.log("DB Connection Config:", config);
 
 module.exports = new Pool(config);
